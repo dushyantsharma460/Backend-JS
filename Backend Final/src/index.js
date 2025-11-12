08ff512f7fbcaa794772965e535ad3e4fb8d4e86
dushyantsharma460 <manuchintubhaibhai@gmail.com>
2025-11-12 11:14:21 +0530

    connect to mongo atlus successfully

---
+++ Backend Final/src/index.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config()
+
+// Now i latest version no need to give path and no need to set experimental excess in package.json
+// dotenv.config({
+//   path: './.env'
+// })
+
+connectDB()
+
+
+// Approach 1 :- (For connecting database)
+/*
+
+import express from "express"
+const app = express()
+
+    (async () => {
+        try {
+            await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+            app.on("error", (e) => {
+                console.log("ERROR", e);
+                throw e
+            })
+            app.listen(process.env.PORT, () => {
+                console.log(`app is listening on port ${process.env.PORT}`)
+            })
+        } catch (error) {
+            console.log("ERROR :", error)
+            throw error
+        }
+    })()
+
+    */
