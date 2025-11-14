f454fe19006223087e3ae87d4fb9e9d33c16f9d6
dushyantsharma460 <manuchintubhaibhai@gmail.com>
2025-11-14 10:46:31 +0530

    error resoled

--- Backend Final/src/utils/ApiError.js
+++ Backend Final/src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
         statusCode,
         message="Something went wrong",
         error=[],
-        statch=""
+        stack=""
     ){
         super(message)
         this.statusCode = statusCode
@@ -13,7 +13,7 @@ class ApiError extends Error {
         this.error = error
 
         if(statch){
-            this.stack = statch
+            this.stack = stack
         } else{
             Error.captureStackTrace(this, this.constructor)
         }
